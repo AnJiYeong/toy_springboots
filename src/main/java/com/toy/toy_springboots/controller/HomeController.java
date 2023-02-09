1e4c7c998c4eb84f880d6e029ffca931ab8dc019
AnJiYeong <memarcis2@naver.com>
2023-02-09 11:46:42 +0900

    security Login

--- src/main/java/com/toy/toy_springboots/controller/HomeController.java
+++ src/main/java/com/toy/toy_springboots/controller/HomeController.java
@@ -1,13 +1,30 @@
 package com.toy.toy_springboots.controller;
 
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
-    
-    @RequestMapping(value = {"", "/", "/main"})
-    public String main(){
-        return "main";
+    @GetMapping("/")
+    public ModelAndView main(ModelAndView modelAndView){
+        SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        if (principal instanceof UserDetails) {
+        String username = ((UserDetails)principal).getUsername();
+        } else {
+        String username = principal.toString();
+        }
+
+        System.out.println(((UserDetails)principal).getUsername());
+
+        String viewName = "/main";
+        modelAndView.setViewName(viewName);
+        return modelAndView;
     }
 }
